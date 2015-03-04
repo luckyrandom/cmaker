3942071bc6ce90fa1bf2630e112a65098c0e1aec
Chenliang Xu <luckyrand@gmail.com>
2015-03-04 02:42:55 -0600

    Add add.hpp to example

---
+++ inst/examples/rcppexample/src/add.hpp
@@ -0,0 +1,8 @@
+#ifndef ADD_HPP
+#define ADD_HPP
+
+double add(double x, double y) {
+  return x + y;
+}
+
+#endif
--- inst/examples/rcppexample/src/rcpp_hello_world.cpp
+++ inst/examples/rcppexample/src/rcpp_hello_world.cpp
@@ -2,12 +2,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+#include "add.hpp"
+
 // [[Rcpp::export]]
 List rcpp_hello_world() {
 
     CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
-    NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
-    List z            = List::create( x, y ) ;
-
+    NumericVector y   = NumericVector::create( 0.0, 1.0, add(3.5, 4.8)) ;
+    List z            = List::create( _["x"] = x, _["y"] = y ) ;
     return z ;
 }

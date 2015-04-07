b73e917bfb67eb5e49e7e340454009be9c75ce51
Chenliang Xu <luckyrand@gmail.com>
2015-04-06 23:41:45 -0500

    rm post_generate_hook
    It's unnecessary complex.

--- R/generate_project.R
+++ R/generate_project.R
@@ -20,8 +20,6 @@ ls_generators <- function() {
          Darwin = generators_mac)
 }
 
-post_generate_hook <- list()
-
 ##' Generate project file for IDE.
 ##'
 ##' Call cmake to generate project file for IDE.
@@ -34,7 +32,5 @@ generate_project <- function(dir, generator,
                              ...) {
   generator <- match.arg(generator, ls_generators())
   cmake(paste("-G", shQuote(generator), "."), file.path(dir, "cmake"), ...)
-  if (generator %in% names(post_generate_hook)) {
-    post_generate_hook[[generator]]()
-  }
+
 }

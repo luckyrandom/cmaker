111a0bcaa5595dd154358d1de38d0cd24f99e470
Chenliang Xu <luckyrand@gmail.com>
2015-04-22 23:29:21 -0500

    rm test of pkg_name for cmaker itself
    meaning of "../.." is not clear, since the pwd is not clear.

--- tests/testthat/test-pkg_name.R
+++ tests/testthat/test-pkg_name.R
@@ -2,8 +2,6 @@ context("pkg name")
 
 test_that("get package name", {
             print(getwd())
-            expect_equivalent(pkg_name("../.."),
-                              "cmaker")
             expect_equivalent(pkg_name(system.file("examples/rcppexample", package = "cmaker")),
                               "rcppexample")
           })

1e5823786f666bd26c28c3e67ba98ca7c4071293
Chenliang Xu <luckyrand@gmail.com>
2015-04-22 22:59:42 -0500

    export function cmaker_command_path
    as it is used in args of some functions

--- R/command_path.R
+++ R/command_path.R
@@ -4,6 +4,7 @@
 ##' otherwise find command with Sys.which
 ##' @param command command name
 ##' @return path of command
+##' @export
 ##' @author Chenliang Xu
 cmaker_command_path <- function(command)
   getOption(sprintf("cmaker.%s.path", command),

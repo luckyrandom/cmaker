b69b1e93d55ed23fcf97e0a380b278bd85c8b08d
Chenliang Xu <luckyrand@gmail.com>
2015-04-24 16:10:45 -0500

    rm debug print

--- tests/testthat/test-pkg_name.R
+++ tests/testthat/test-pkg_name.R
@@ -1,7 +1,6 @@
 context("pkg name")
 
 test_that("get package name", {
-            print(getwd())
             expect_equivalent(pkg_name(system.file("examples/rcppexample", package = "cmaker")),
                               "rcppexample")
           })
